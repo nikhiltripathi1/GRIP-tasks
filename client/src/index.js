ae03add811c15856ad62e2217b203fcb83da7743
nikhil tripathi <tripathin645@gmail.com>
2020-09-29 15:26:58 +0530

    'task-2'

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <Router basename="/*">
+  <Router basename={window.location.pathname}>
     <App />
   </Router>,
   document.getElementById("root")
